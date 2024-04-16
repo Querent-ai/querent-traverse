a8333847aea86b0188631182479c1b9fa8c1fde4
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-04-16 15:36:52 -0500

    base work for querent analytics client

---
+++ src/routes/(no-sidebar)/playground/stacked/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Playground from './Playground.svelte';
+	import MetaTag from '../../../utils/MetaTag.svelte';
+
+	const path: string = '/playground/stacked';
+  const description: string = 'Playground stacked example - Querent Admin Dashboard';
+	const metaTitle: string = 'Querent Admin Dashboard - Playground stacked';
+  const subtitle: string = 'Playground stacked';
+</script>
+
+<MetaTag {path} {description} title={metaTitle} {subtitle} />
+
+<div
+	id="main-content"
+	class="relative mx-auto h-full w-full max-w-screen-2xl overflow-y-auto bg-gray-50 dark:bg-gray-900"
+>
+	<Playground />
+</div>
