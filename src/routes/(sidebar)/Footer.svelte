7177d71edbd99f43feb15c48ee9c28d777ba953d
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-07-23 12:18:16 -0500

    All rights reserved

--- src/routes/(sidebar)/Footer.svelte
+++ src/routes/(sidebar)/Footer.svelte
@@ -54,7 +54,7 @@
 </Frame>
 
 <p class="my-10 text-center text-sm text-gray-500">
-	© 2019-2023 <a href="https://flowbite.com/" class="hover:underline" target="_blank">
-		Flowbite.com
+	© 2023-2024 <a href="https://querent.xyz/" class="hover:underline" target="_blank">
+		Querent.xyz
 	</a>. All rights reserved.
 </p>
--- src/routes/(sidebar)/Sidebar.svelte
+++ src/routes/(sidebar)/Sidebar.svelte
@@ -25,7 +25,7 @@
 	const closeDrawer = () => {
 		drawerHidden = true;
 	};
-
+	let fabric_icon = new URL('assets/images/fabric_icon.png', import.meta.url).href;
 	let iconClass =
 		'flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white';
 	let itemClass =
